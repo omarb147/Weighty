5cf0f97c5832bdcf1df3013f4619d5010456583a
obello147 <o_bello@live.co.uk>
2019-08-22 21:09:03 +0100

    table ready for input from datapoints

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,34 +1,45 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+
 const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
+  options: {
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          distribution: "linear",
+          bounds: "data",
+          time: { stepSize: 0.3 }
+        }
+      ]
     }
-  ]
+  },
+
+  data: {
+    datasets: [
+      {
+        label: "Demo",
+        data: [
+          {
+            t: new Date("2015-3-15 13:3"),
+            y: 12
+          },
+          {
+            t: new Date("2015-3-25 13:2"),
+            y: 21
+          },
+          {
+            t: new Date("2015-4-25 14:12"),
+            y: 32
+          }
+        ]
+      }
+    ]
+  }
 };
 
 function Graph() {
-  return <Line data={data} />;
+  return <Line {...data} />;
 }
 
 export default Graph;
